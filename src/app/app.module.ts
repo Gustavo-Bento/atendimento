65c7c05e0f57c2b5f5d694ce3a7a039c3ebe119c
Gustavo Ferreira <gfmb94@gmail.com>
2024-10-18 12:27:52 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AtendimentoComponent } from './atendimento/atendimento.component';
+import { OcorrenciasComponent } from './ocorrencias/ocorrencias.component';
+import { VeiculosComponent } from './veiculos/veiculos.component';
+import { FuncionariosComponent } from './funcionarios/funcionarios.component';
+import { EquipesComponent } from './equipes/equipes.component';
+import { TurnosComponent } from './turnos/turnos.component';
+import { AtendimentosVeiculosComponent } from './atendimentos-veiculos/atendimentos-veiculos.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AtendimentoComponent,
+    OcorrenciasComponent,
+    VeiculosComponent,
+    FuncionariosComponent,
+    EquipesComponent,
+    TurnosComponent,
+    AtendimentosVeiculosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/atendimentos-veiculos/atendimentos-veiculos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atendimentos-veiculos',
+  templateUrl: './atendimentos-veiculos.component.html',
+  styleUrls: ['./atendimentos-veiculos.component.css']
+})
+export class AtendimentosVeiculosComponent {
+
+}
